a8d35833f62eb30db7c3ef839604196c30735b2f
marcioalvesramos90@hotmail.com <marcioalvesramos90@hotmail.com>
2018-07-25 10:38:34 -0300

    modulo 2 rockettseat finalizado

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './styles/global';
+
+import Main from './pages/Main';
+
+export default () => <Main />;
---
+++ src/pages/Main/index.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+import { Container, Form } from './styles';
+import logo from '../../assets/logo.png';
+
+import CompareList from '../../components/CompareLists';
+import api from '../../services/api';
+
+class Main extends Component {
+  state = {
+    loading: false,
+    repositoryError: false,
+    repositoryInput: '',
+    repositories: [],
+  };
+
+  handleAddRepository = async (e) => {
+    const { repositoryInput, repositories } = this.state;
+    e.preventDefault();
+    this.setState({ loading: true });
+    try {
+      const { data: repository } = await api.get(`/repos/${repositoryInput}`);
+
+      repository.lastCommit = moment(repository.pushed_at).fromNow();
+
+      this.setState({
+        repositoryInput: '',
+        repositories: [...repositories, repository],
+        repositoryError: false,
+      });
+    } catch (err) {
+      this.setState({ repositoryError: true });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  render() {
+    const {
+      repositories, repositoryInput, repositoryError, loading,
+    } = this.state;
+    return (
+      <Container>
+        <img src={logo} alt="Github Compare" />
+        <Form withError={repositoryError} onSubmit={this.handleAddRepository}>
+          <input
+            type="text"
+            placeholder="usuario/repositório"
+            value={repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">{loading ? <i className="fa fa-spinner fa-pulse" /> : 'ok'}</button>
+        </Form>
+        <CompareList repositories={repositories} />
+      </Container>
+    );
+  }
+}
+
+export default Main;
